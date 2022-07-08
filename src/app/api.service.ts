e89bf386ea226e76682e9e088f1653548fc9659d
annapurna1702 <annapurnapadmaprema@gmail.com>
2022-07-08 15:42:55 +0530

    Updated todo

---
+++ src/app/api.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+
+  viewTodo=()=>{
+
+    return this.http.get("https://jsonplaceholder.typicode.com/todos")
+  }
+  
+
+
+}
